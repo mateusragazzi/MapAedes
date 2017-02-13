fcab7c730dd4a3d31be75413b36d6c57cb0ca693
mateusragazzi <mateusragazzi.b@gmail.com>
2017-02-13 18:07:44 -0300

    Organização em Pastas

--- app/src/main/java/com/example/mateus/mapaedes/Activities/Main.java
+++ app/src/main/java/com/example/mateus/mapaedes/Activities/Main.java
@@ -1,4 +1,4 @@
-package com.example.mateus.mapaedes;
+package com.example.mateus.mapaedes.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.mateus.mapaedes.R;
 
 public class Main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
